4f647d2076942b2b3cff97cf73967355c0d15469
amanmamgain9 <amanmamgain9@gmail.com>
2024-02-21 23:12:36 +0530

    adding a lil style

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VideoUploader from './VideoPlayer';
+import { Container, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
+import { BsUpload } from 'react-icons/bs';
+import { MdRecordVoiceOver } from 'react-icons/md';
 
-function App() {
 
-    
+function App(){
     return (
         <div className="App">
           <header className="App-header">
-            <p>
-              Upload a video to process with ffmpeg.wasm
-            </p>
+            <h1>Yazzy Captioner</h1>
+            <p style={{marginTop:10, fontSize:20}}>Upload a video to caption</p>
             <VideoUploader />
           </header>
+          <Container>
+            <Row className="justify-content-md-center">
+              <Col md="auto">
+                <ButtonGroup aria-label="File upload options">
+                  <Button variant="outline-primary">From URL</Button>
+                  <Button variant="primary">
+                    <BsUpload /> From file
+                  </Button>
+                  <Button variant="outline-primary">
+                    <MdRecordVoiceOver /> Record
+                  </Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
+          </Container>
         </div>
     );
 }
